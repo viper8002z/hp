3dfb52e2c24c476d632837179a6e0b89d2263a1e
viper8002z <viper8002z@gmail.com>
2025-08-15 01:30:31 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-const app = express();
 const PORT = process.env.PORT || 10000;
+const proxy = httpProxy.createProxyServer({});
 
-// Generic HTTP/HTTPS proxy: forwards all requests to the requested host
-app.use('/', createProxyMiddleware({
-  target: 'http://localhost',   // Placeholder, will be overridden by req.url
-  changeOrigin: true,
-  router: (req) => {
-    // Construct target dynamically based on full original URL
-    return req.url.startsWith('http') ? req.url : `http://${req.headers.host}${req.url}`;
-  },
-  logLevel: 'info',
-  secure: false,
-}));
-
-app.listen(PORT, () => {
-  console.log(`Generic HTTP/HTTPS proxy running on port ${PORT}`);
+const server = http.createServer((req, res) => {
+  proxy.web(req, res, { target: req.url, changeOrigin: true }, err => {
+    res.writeHead(502);
+    res.end("Bad gateway: " + err.message);
+  });
 });
+
+// Handle HTTPS CONNECT method for tunneling
+server.on('connect', (req, clientSocket, head) => {
+  const { port, hostname } = new URL(`http://${req.url}`);
+  const serverSocket = require('net').connect(port || 443, hostname, () => {
+    clientSocket.write('HTTP/1.1 200 Connection Established\r\n\r\n');
+    serverSocket.write(head);
+    serverSocket.pipe(clientSocket);
+    clientSocket.pipe(serverSocket);
+  });
+});
+
+server.listen(PORT, () => console.log(`Generic HTTP/HTTPS proxy running on port ${PORT}`));
